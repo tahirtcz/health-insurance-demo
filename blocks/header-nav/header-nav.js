3a641950fb87a411c473739d7800165facf5252b
tahir <edstechchefz@gmail.com>
2025-04-29 17:59:44 +0530

    fix

--- blocks/header-nav/header-nav.js
+++ blocks/header-nav/header-nav.js
@@ -1,7 +1,11 @@
 export default function decorate(block) {
     const container = document.createElement('div');
     container.setAttribute('class', 'container');
-  
+    
+      const  leftText = block.querySelector("div > div > div > div:nth-child(1) > p").textContent;
+      const rightImage = block.querySelector("div > div > div > div:nth-child(2) > p > picture > img").src;
+      const rightText = block.querySelector("div > div > div > div:nth-child(2) > p").textContent;
+     
     const sectionEl = document.createElement('section');
     sectionEl.setAttribute('class', 'JFM_Banner d-flex');
     sectionEl.setAttribute('style', 'content-visibility: auto; ');
@@ -11,15 +15,13 @@ export default function decorate(block) {
 
         const pEl = document.createElement('p');
         pEl.setAttribute('class', 'mobile_hide');
-  
+          
           const spanEl = document.createElement('span');
-          spanEl.textContent = 'Save tax up to ₹75,000~ u/s 80D';
-            const supEl = document.createElement('sup');
-            supEl.textContent = '';
-          spanEl.append(supEl);
-        pEl.append(spanEl);
+          spanEl.textContent = leftText;
+          pEl.append(spanEl);
+       
       articleEl.append(pEl);
-  
+
         const asideEl = document.createElement('aside');
         asideEl.setAttribute('class', 'request-a-callback d-flex');
   
@@ -27,19 +29,19 @@ export default function decorate(block) {
           svgEl.setAttribute('width', '24');
           svgEl.setAttribute('height', '24');
   
-          svgEl.setAttribute('src', "https://cdn3.iconfinder.com/data/icons/pyconic-icons-1-2/512/phone-call-active-1024.png");
+          svgEl.setAttribute('src', rightImage);
         asideEl.append(svgEl);
   
           const aEl = document.createElement('a');
           aEl.setAttribute('href', 'tel:1800-102-4499');
           aEl.setAttribute('id', 'buypolicy_tollfree');
-          aEl.textContent = 'Buy New Policy :1800-102-4499';
+          aEl.textContent = rightText;
         asideEl.append(aEl);
       articleEl.append(asideEl);
     sectionEl.append(articleEl);
     container.append(sectionEl);
   
-  
+   
     block.textContent = '';
     block.append(container);
   }
