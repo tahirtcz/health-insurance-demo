aed5d7ced23138ea4a27cdd84834f8de29d82528
tahirtcz <tahir@techchefz.com>
2025-05-15 23:53:07 +0530

    header nav changes

--- blocks/header-nav/header-nav.js
+++ blocks/header-nav/header-nav.js
@@ -1,47 +1,62 @@
 export default function decorate(block) {
-    const container = document.createElement('div');
-    container.setAttribute('class', 'container');
-    
-      // const  leftText = block.querySelector("div > div > div > div:nth-child(1) > p").textContent;
-      // const rightImage = block.querySelector("div > div > div > div:nth-child(2) > p > picture > img").src;
-      // const rightText = block.querySelector("div > div > div > div:nth-child(2) > p").textContent;
-     
-    const sectionEl = document.createElement('section');
-    sectionEl.setAttribute('class', 'JFM_Banner d-flex');
-    sectionEl.setAttribute('style', 'content-visibility: auto; ');
-  
-      const articleEl = document.createElement('article');
-      articleEl.setAttribute('class', 'container d-flex');
+  const container = document.createElement('div');
+  container.setAttribute('class', 'container');
 
-        const pEl = document.createElement('p');
-        pEl.setAttribute('class', 'mobile_hide');
-          
-          const spanEl = document.createElement('span');
-          spanEl.textContent = "Save tax up to ₹75,000~ u/s 80D";
-          pEl.append(spanEl);
-       
-      articleEl.append(pEl);
+  const divEl = document.createElement('div');
+  divEl.setAttribute('class', 'section header-nav-container');
+  divEl.setAttribute('data-section-status', 'loaded');
+  divEl.setAttribute('style', '');
 
-        const asideEl = document.createElement('aside');
-        asideEl.setAttribute('class', 'request-a-callback d-flex');
-  
-          const svgEl = document.createElement('img');
-          svgEl.setAttribute('width', '24');
-          svgEl.setAttribute('height', '24');
-  
-          svgEl.setAttribute('src', "https://main--health-insurance-demo--eds-dwao.aem.live/media_181ab96d0bb0c770df1c77981d8421a47f7993db0.png?width=750&format=png&optimize=medium");
-        asideEl.append(svgEl);
-  
-          const aEl = document.createElement('a');
-          aEl.setAttribute('href', 'tel:1800-102-4499');
-          aEl.setAttribute('id', 'buypolicy_tollfree');
-          aEl.textContent = "Buy New Policy :1800-102-4499";
-        asideEl.append(aEl);
-      articleEl.append(asideEl);
-    sectionEl.append(articleEl);
-    container.append(sectionEl);
-  
-   
-    block.textContent = '';
-    block.append(container);
-  }
+    const divEl2 = document.createElement('div');
+    divEl2.setAttribute('class', 'header-nav-wrapper');
+
+      const divEl3 = document.createElement('div');
+      divEl3.setAttribute('class', 'header-nav block');
+      divEl3.setAttribute('data-block-name', 'header-nav');
+      divEl3.setAttribute('data-block-status', 'loaded');
+
+        const divEl4 = document.createElement('div');
+        divEl4.setAttribute('class', 'container');
+
+          const sectionEl = document.createElement('section');
+          sectionEl.setAttribute('class', 'JFM_Banner d-flex');
+          sectionEl.setAttribute('style', 'content-visibility: auto; ');
+
+            const articleEl = document.createElement('article');
+            articleEl.setAttribute('class', 'container d-flex');
+
+              const pEl = document.createElement('p');
+              pEl.setAttribute('class', 'mobile_hide');
+
+                const spanEl = document.createElement('span');
+                spanEl.textContent = 'Save tax up to ₹75,000~ u/s 80D';
+              pEl.append(spanEl);
+            articleEl.append(pEl);
+
+              const asideEl = document.createElement('aside');
+              asideEl.setAttribute('class', 'request-a-callback d-flex');
+
+                const imgEl = document.createElement('img');
+                imgEl.setAttribute('width', '24');
+                imgEl.setAttribute('height', '24');
+                imgEl.setAttribute('src', 'https://main--health-insurance-demo--eds-dwao.aem.live/media_181ab96d0bb0c770df1c77981d8421a47f7993db0.png?width=750&format=png&optimize=medium');
+                imgEl.setAttribute('loading', 'eager');
+              asideEl.append(imgEl);
+
+                const aEl = document.createElement('a');
+                aEl.setAttribute('href', 'tel:1800-102-4499');
+                aEl.setAttribute('id', 'buypolicy_tollfree');
+                aEl.textContent = 'Buy New Policy :1800-102-4499';
+              asideEl.append(aEl);
+            articleEl.append(asideEl);
+          sectionEl.append(articleEl);
+        divEl4.append(sectionEl);
+      divEl3.append(divEl4);
+    divEl2.append(divEl3);
+  divEl.append(divEl2);
+  container.append(divEl);
+
+
+  block.textContent = '';
+  block.append(container);
+}
