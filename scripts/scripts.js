0de30604afdf8ff083eb9d79b0a49070aff46cdf
tahirtcz <tahir@techchefz.com>
2025-05-16 00:06:12 +0530

    changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -74,6 +74,7 @@ export function decorateMain(main) {
 async function loadEager(doc) {
   document.documentElement.lang = 'en';
   decorateTemplateAndTheme();
+  loadHeader(doc.querySelector('header'));
   const main = doc.querySelector('main');
   if (main) {
     decorateMain(main);
@@ -103,7 +104,6 @@ async function loadLazy(doc) {
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector('header'));
   loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
